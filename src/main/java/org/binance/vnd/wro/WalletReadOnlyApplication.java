bcd937571ebc4e308cf704de137495120be56451
vladian.diac <diac.vladian@gmail.com>
2023-01-02 22:31:27 +0100

    Inception

---
+++ src/main/java/org/binance/vnd/wro/WalletReadOnlyApplication.java
@@ -0,0 +1,13 @@
+package org.binance.vnd.wro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletReadOnlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletReadOnlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/binance/vnd/wro/WalletReadOnlyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.binance.vnd.wro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletReadOnlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
